de2e53c64863035fdcdb890f79e812ace7b8b105
mxdavis <md01@me.com>
2017-09-14 12:56:57 +0300

    import custom css

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './styles/uikit.css';
+// import 'react-select/dist/react-select.css'
+import './styles/custom.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
