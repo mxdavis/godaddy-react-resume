03cbfafc7d1ced874a72ee61e551ae498d0fc18d
mxdavis <md01@me.com>
2017-09-14 14:58:05 +0300

    create basic containers for pages

---
+++ src/components/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Experience = () =>  {
+  return (
+    <div className="App">
+      Experience
+    </div>
+  )
+}
+
+
+export default Experience;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () =>  {
+  return (
+    <div className="App">
+      You are Home
+    </div>
+  )
+}
+
+
+export default Home;
