b06bf7a05ee0621e6d07a29e6d964948dfeff9ce
mxdavis <md01@me.com>
2017-09-18 23:42:01 +0300

    adjust language sizes

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -10,22 +10,22 @@ const Languages = () =>  {
   return (
     <div className="App main">
       <div className="text-padded green-text">
-        <h1>Some of My Faves</h1>
+        <h1>Favorite Technologies</h1>
         <div className="uk-grid">
           <div className="uk-width-1-5 slight-padding">
-            <img src={RubyLogo} alt="Ruby" />
+            <img src={RubyLogo} alt="Ruby" height="50%" width="50%"/>
           </div>
           <div className="uk-width-1-5 slight-padding">
-            <img src={RailsLogo} alt="Rails" />
+            <img src={RailsLogo} alt="Rails" height="50%" width="50%" />
           </div>
           <div className="uk-width-1-5 slight-padding">
-            <img src={JSLogo} alt="JS" />
+            <img src={JSLogo} alt="JS" height="50%" width="50%"/>
           </div>
           <div className="uk-width-1-5 slight-padding">
-            <img src={ReactLogo} alt="React" />
+            <img src={ReactLogo} alt="React" height="50%" width="50%"/>
           </div>
           <div className="uk-width-1-5 slight-padding">
-            <img src={ReduxLogo} alt="Redux" />
+            <img src={ReduxLogo} alt="Redux" height="50%" width="50%"/>
           </div>
         </div>
       </div>
