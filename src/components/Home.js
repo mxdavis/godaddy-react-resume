87b1a743e59dfa20241827d3971ade8f933f73e2
mxdavis <md01@me.com>
2017-09-18 15:35:59 +0300

    update home with languages, portfolio and text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,40 @@
 import React from 'react';
 
+import Portfolio from './Portfolio'
+import Languages from './Languages'
+import projects from '../data/projects'
+
 import Wave from '../images/Wave.png'
-import BuildingThings from '../images/BuildingThings.png'
-import Graduate from '../images/Graduate.png'
-import Flatiron from '../images/Flatiron.png'
+import GoDaddyLogo from '../images/GoDaddyLogo.png'
+
 
 const Home = () =>  {
   return (
       <div className="App main">
 
         <div className="text-padded">
-          <h1>Hi <img src={Wave} alt="wave" width='5%' height='5%'/>  I'm Malki!</h1>
+          <h1>Hi <img src={Wave} alt="wave" width='5%' height='5%'/>  I'm Malki and </h1>
+          <h1 className="no-bottom-padding">I would love to work for <img src={GoDaddyLogo} alt="GoDaddyLogo" width='25%' height='25%'/></h1>
 
           <h3>
-            I started my career as an administrative assistant, and worked my way up to head the IT department. As a recent <img src={Graduate} alt="graduate" width='3%' height='3%'/> graduate of the Flatiron School <img src={Flatiron} alt="flatiron" width='2%' height='2%'/>, and with a passion to code 👩‍💻 and learn, I'm looking forward to do the same in software development and have a future of building great things.
+            GoDaddy has an awesome community and encourages team work. I thrive from being a team player, and working in a positive and friendly atmosphere. <br/>
+            I have lots of experience working remotely: I currently am a Remote Tech Coach for Flatiron School; I am an IT Consultant for small businesses remotely; I run a weekly remote JavaScript meetup where we tackle some CodeWars together; I attended the online Flatiron School where I made many friends, and attended many online meetings, starting off our week with "Motivation Monday" and ending with "Thoughtful Thursday".
+            <br/>
+            I love learning new things, and to be challenged. GoDaddy's innovative technology will keep me learning, and solving new problems daily. <br />If GoDaddy would hire me I look forward to a future of building great things!
           </h3>
          </div>
 
         <div className="gray">
-          <img src={BuildingThings} alt="Building Things" width='60%'/>
+          <div className="text-padded green-text">
+            <h1> Some Things I've Built</h1>
+            <div className="uk-grid">
+              {projects.map(project => <Portfolio project={project}/>)}
+            </div>
+          </div>
+         </div>
+
+         <Languages />
         </div>
-       </div>
   )
 }
 
